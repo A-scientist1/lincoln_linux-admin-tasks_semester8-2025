69a1dcc1f8c97f86a5d5067a74c358d90a780f59
A-scientist1 <idakpoworld@gmail.com>
2025-09-15 13:03:29 +0000

    Corrected more syntax errors for the script that creates 5 users

--- auto-create-5-UsersUnderDevTeam.sh
+++ auto-create-5-UsersUnderDevTeam.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 sudo groupadd -f devteam
 echo "attempting to create group  'devteam'"
@@ -6,7 +7,7 @@ if getent group devteam > /dev/null; then
 	echo "Do you want to create users with 'default' names (user1-user5) or 'custom' name?"
 	read -p "Enter 'default' or 'custom':" user_choice
 
-	if [["$user_choice" =~ ^[Dd]efault$]]; then
+	if [[ "$user_choice" =~ ^[Dd]efault$ ]]; then
 		for i in {1..5}; do
 			username="user$i"
 			sudo useradd -m -G devteam "$username"
@@ -16,8 +17,8 @@ if getent group devteam > /dev/null; then
 		done
 		echo "All 5 users have been created with default usernames."
 
-	elif [["$user_choice" =~ ^[Cc]ustom$]]]; then
-		for i in {seq 1 5}; do
+	elif [[ "$user_choice" =~ ^[Cc]ustom$ ]]; then
+		for i in $(seq 1 5); do
 			read -p "Enter a name for user$i:" username
 			sudo useradd -m -G devteam "$username"
 			echo "$username:12345678pass" | sudo chpasswd
